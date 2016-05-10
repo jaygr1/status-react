2aa2b0bc03f2a05ea631c75c4c254189ffd692fd
Jay Ribakove <jrib6@yahoo.com>
2016-05-10 11:06:16 -0400

    wip

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,36 @@
 var React = require('react');
+var Environments = require('../components/Environments');
+
+// var Home = React.createClass({
+//   render: function() {
+//     return (
+//       <div> Home </div>
+//     )
+//   }
+// });
 
 var Home = React.createClass({
-  render: function() {
+  render: function () {
+    debugger;
+    var reposInfo = this.props.reposInfo || [];
+    var repos = reposInfo[0] || [];
+    debugger;
+    var environmentsInfo = this.props.environmentsInfo || [];
+    var environments = environmentsInfo[0] || [];
     return (
-      <div> Home </div>
+      <ul>
+        {repos.map(function(repo){
+          return <li key={repo.id} style={styles.li}><a href={repo.url}> {repo.name} </a></li>
+          console.log('this is the right place')
+            {environments.map(function(environment){
+              return repo.id === environment.id
+              ? <li header={'SHOW UP'} key={environment.id}><a href={environment.url}> {environment.name} words</a></li>
+              : <li> Nothing </li>
+              })
+            }
+          })
+        }
+      </ul>
     )
   }
 });
--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -1,12 +1,64 @@
 var React = require('react');
-var Home = require('../components/home');
+var Home = require('../components/Home');
+var Environments = require('../components/Environments');
+var AppsHelper = require('../utils/ww-apps-helper');
+var ReposContainer = require('../containers/getReposContainer');
+var EnvironmentsContainer = require('../containers/EnvironmentContainer');
 
-var Home = React.createClass({
+// var Home = React.createClass({
+//   render: function() {
+//     return (
+//       <div> Home </div>
+//     )
+//   }
+// });
+
+var HomeContainer = React.createClass({
+  // contextTypes: {
+  //   router: React.PropType.object.isRequired
+  // },
+  getInitialState: function (){
+    return {
+      reposInfo: [],
+      environmentsInfo: []
+    }
+  },
+  // componentDidMount: function() {
+  //   debugger;
+  //   AppsHelper.Repos()
+  //     .then(function(repos){
+  //       this.setState({
+  //         reposInfo: [repos]
+  //       })
+  //     }.bind(this))
+  //   },
+  //   AppsHelper.Environments()
+  //     .then(function(environments){
+  //       this.setState({
+  //         environmentsInfo: [environments]
+  //       })
+  //     }.bind(this))
+  //   }
+  // },
+  componentDidMount: function () {
+    return (
+      <div>
+      <ReposContainer
+        reposInfo={this.state.reposInfo}
+      />
+      debugger;
+      <EnvironmentsContainer
+        environmentsInfo={this.state.environmentsInfo}/>
+      </div>
+    )
+  },
+
+
   render: function() {
     return (
-      <div>Home </div>
+      <div> <ReposContainer/> < EnvironmentsContainer /> </div>
     )
   }
 });
 
-module.exports = Home;
+module.exports = HomeContainer;
