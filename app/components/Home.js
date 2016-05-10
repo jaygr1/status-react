d5936a69979375cd4d50fee0338f1bc438219f15
Jay Ribakove <jrib6@yahoo.com>
2016-05-10 17:24:01 -0400

    styling wip

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,61 +1,4 @@
 var React = require('react');
-// var Environments = require('../components/Environments');
-
-// var Home = React.createClass({
-//   render: function() {
-//     return (
-//       <div> Home </div>
-//     )
-//   }
-// });
-
-// var homeTable = function homeTable (repos, environments) {
-//
-//   return repos.map(function(repo){
-//
-//     repo.length > 0
-//     ? <li key={repo.id}><a href={repo.url}> {repo.name} </a></li>
-//     : <div> Nada </div>
-//
-//     environments.map(function(environment){
-//       return environment
-//       repo.id === environment.id
-//       ? <li key={environment.id}><a href={environment.url}> {environment.name}</a></li>
-//       : <li> Nothing </li>
-//     })
-//
-//   })
-//
-// }
-
-// var Home = React.createClass({
-//   render: function () {
-//     // debugger;
-//     var reposInfo = this.props.reposInfo[0] || [];
-//     debugger;
-//     var repos = reposInfo || [];
-//     debugger;
-//
-//     var environmentsInfo = this.props.environmentsInfo || [];
-//     debugger;
-//     var environments = environmentsInfo[0] || [];
-//     debugger;
-//
-//     return (
-//       <ul>
-//
-//       {repos.map(function(repo){
-//         return <li key={repo.id}><a href={repo.url}> {repo.name} </a></li>
-//
-//         })
-//       };
-//
-//
-//
-//       </ul>
-//     )
-//   }
-// });
 
 var Home = React.createClass({
 
@@ -66,7 +9,7 @@ var Home = React.createClass({
     var repoEnvs = environments.filter(function(environment){
       return environment.repo_id === repoId;
     }).map(function (repoEnv) {
-      return <li key={repoEnv.id}><a href={repoEnv.url}> {repoEnv.name} </a></li>
+      return <tr class="col-md-4" key={repoEnv.id}><a href={repoEnv.url}> {repoEnv.name} </a></tr>
     })
 
     return repoEnvs;
@@ -85,16 +28,11 @@ var Home = React.createClass({
     debugger;
 
     return (
-      <ul>
-
+      <table>
       {repos.map((repo) => {
-        return <li key={repo.id}><a href={repo.url}> {repo.name} {this.getEnvironmentsForRepo(repo.id)} </a></li>
-      })
-      }
-
-
-
-      </ul>
+        return <tr key={repo.id}><a href={repo.url}> <th>{repo.name}</th><tr> {this.getEnvironmentsForRepo(repo.id)}<br/></tr> </a></tr>
+      })}
+      </table>
     )
   }
 });
