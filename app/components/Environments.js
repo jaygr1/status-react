4989165f8e70b4aee7a746a3cc0a412efe360def
Jay Ribakove <jrib6@yahoo.com>
2016-05-11 09:19:12 -0400

    wip

--- app/components/Environments.js
+++ app/components/Environments.js
@@ -12,10 +12,16 @@ var Environments = React.createClass({
     var environments = environmentsInfo[0] || [];
     return (
       <ul>
+        <table className='table table-striped'>
+        <h2> Environments </h2>
+        <tbody className='table table-hover'>
+        <div><strong><span className="col-sm-4"> Name </span> <span className="col-sm-4"> Status </span> <span className="col-sm-4"> Last Updated </span></strong> </div>
         {environments.map(function(environment){
-          return <li key={environment.id}><a href={environment.url}> {environment.name} </a></li>
+          return <div><a key={environment.id} href={environment.url}> <span className="col-sm-4">{environment.name}</span></a> <span className="col-sm-4 ">{environment.status}</span> <span className="col-sm-4 ">{environment.updated_at}</span></div>
           })
         }
+          </tbody>
+        </table>
       </ul>
     )
   }
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,22 @@
 var React = require('react');
 var styles = require('../styles/main');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Header = React.createClass({
   render: function() {
     return (
-      <h1 style={styles.header} styles={styles.text}>App Status</h1>
+      <h1 style={styles.header} styles={styles.text}>
+        <span className="navbar-brand"><Link to='/'>
+            Home
+        </Link></span>
+        <span className="navbar-brand"><Link to='/environments'>
+            Environments
+        </Link></span>
+        <span className="navbar-brand"><Link to='/repos'>
+            Repos
+        </Link></span>
+      </h1>
     )
   }
 });
