43aff0777b60dd069e1cb27c546208d14999beeb
Jay Ribakove <jrib6@yahoo.com>
2016-05-10 23:27:03 -0400

    wip styling

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var TimeAgo = require('react-timeago');
 
 var Home = React.createClass({
 
@@ -9,28 +10,26 @@ var Home = React.createClass({
     var repoEnvs = environments.filter(function(environment){
       return environment.repo_id === repoId;
     }).map(function (repoEnv) {
-      return <tr class="col-md-4" key={repoEnv.id}><a href={repoEnv.url}> {repoEnv.name} </a></tr>
+      return <div><a key={repoEnv.id} href={repoEnv.url}> <span className="col-sm-4">{repoEnv.name}</span></a> <span className="col-sm-4 ">{repoEnv.status}</span> <span className="col-sm-4 ">{repoEnv.updated_at}</span></div>
     })
 
     return repoEnvs;
   },
 
   render: function () {
-    // debugger;
     var reposInfo = this.props.reposInfo[0] || [];
-    debugger;
     var repos = reposInfo || [];
-    debugger;
 
     var environmentsInfo = this.props.environmentsInfo || [];
-    debugger;
     var environments = environmentsInfo[0] || [];
     debugger;
 
     return (
-      <table>
+      <table className="table table-striped">
       {repos.map((repo) => {
-        return <tr key={repo.id}><a href={repo.url}> <th>{repo.name}</th><tr> {this.getEnvironmentsForRepo(repo.id)}<br/></tr> </a></tr>
+        return <tbody><div className="table-striped table-hover"><tr key={repo.id}><a href={repo.url}> <h3><strong><thead>{repo.name}</thead></strong></h3> </a></tr>
+        <div><strong><span className="col-sm-4"> Name </span> <span className="col-sm-4"> Status </span> <span className="col-sm-4"> Last Updated </span></strong> </div>
+        {this.getEnvironmentsForRepo(repo.id)}<br/></div></tbody>
       })}
       </table>
     )
